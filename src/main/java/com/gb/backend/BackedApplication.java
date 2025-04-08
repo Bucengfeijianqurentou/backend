744162115ad37c6cb943d73b85932e670a43896f
Bucengfeijianqurentou <2193008417@qq.com>
2025-04-08 16:02:25 +0800

    配置数据库

--- src/main/java/com/gb/backend/BackedApplication.java
+++ src/main/java/com/gb/backend/BackedApplication.java
@@ -1,4 +1,4 @@
-package com.gb.backed;
+package com.gb.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/gb/backend/BackedApplicationTests.java
+++ src/test/java/com/gb/backend/BackedApplicationTests.java
@@ -1,4 +1,4 @@
-package com.gb.backed;
+package com.gb.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
